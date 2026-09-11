c464f4ef8eedff51be93ead594e2e67d3f5b75ca
Gustavo Jimenez <gustavojimenez@Gustavos-MacBook-Air.local>
2026-09-11 08:24:22 -0500

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8085/'
+  // apiUrl: 'http://localhost:8085/'
+  apiUrl: 'https://casa-de-moda.herokuapp.com/'
   
 };
 
